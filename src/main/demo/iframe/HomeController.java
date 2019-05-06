22306945fa2f307cff1344eb587eb11dc47d9b67
newPlayer <447880968@qq.com>
2019-05-06 22:05:22 +0800

    home

--- src/main/demo/iframe/HomeController.java
+++ src/main/demo/iframe/HomeController.java
@@ -8,7 +8,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -18,12 +21,18 @@ public class HomeController {
     public String display(HttpServletRequest req){
         String reqType = req.getParameter("param");
         System.out.println("reqTyp: " + reqType);
-        Map<String, String> map = new HashMap<String, String>();
-        map.put("a","1");
-        map.put("b","2");
+        Map<String, Integer> map = new HashMap<String, Integer>();
+        Map<String, Integer> map2 = new HashMap<String, Integer>();
+        map.put("moka",28);
+        map.put("Latte",26);
+        map.put("Capucino", 123);
+        map2.put("Tilamisu", 20);
+        map2.put("Cheese", 22);
+        List list = new ArrayList();
+        list.add(map);
+        list.add(map2);
         Gson gson = new Gson();
-        String str = gson.toJson(map);
-
+        String str = gson.toJson(list);
         return str;
     }
 }
